cafa5faf451595110fb6b7377b2d6b07c74c3832
przemek <przemek.bialkwno@gmail.com>
2026-09-08 19:42:00 +0200

    creating Logo

--- ui/Logo.tsx
+++ ui/Logo.tsx
@@ -2,11 +2,18 @@ import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Link
-      href="/"
-      className="text-xl font-semibold tracking-[0.2em] text-white"
-    >
-      NOVAHOME
+    <Link href="/" className="flex flex-col">
+      <div className="font-serif text-3xl font-semibold leading-none tracking-tight text-foreground">
+        <span className="font-normal italic">Piotr</span> Cywiński
+      </div>
+      <div className="my-2 h-px w-full bg-white opacity-30" />
+      <div className="whitespace-nowrap text-[8px] font-semibold tracking-widest">
+        SPRZEDAŻ
+        <span className="mx-2 opacity-50">•</span>
+        DORADZTWO
+        <span className="mx-2 opacity-50">•</span>
+        ZARZĄDZANIE
+      </div>
     </Link>
   );
 };
